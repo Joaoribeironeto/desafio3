59ff3d7cb7d425699d1d43a7937332dbbf37998a
João Ribeiro <67529204+Joaoribeironeto@users.noreply.github.com>
2024-05-29 12:27:34 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+List<int> lista = new List<int> { 1, 5, 10, 2, 6, 11 };
+
+int somaLista = 0;
+
+foreach (int soma in lista)
+{
+    somaLista += soma;
+}
+
+Console.WriteLine($"A soma dos elementos é {somaLista}");
